e930b2d1f96ec90afd310085bde40a6eee170319
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-02 11:02:25 +0530

    Flipcoin

---
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using FunctionalProgram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class BasicPraograms
+    {
+        static void Main(string[] args)
+        {
+            Flipcoin flipCoin = new Flipcoin();
+            flipCoin.Flip();
+        }
+    }
+}
