31736f4e9468f754bd6dfb5b945fc8ef1b006a81
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-02 13:36:56 +0530

    Leapyear

---
+++ FunctionalProgram/FunctionalProgram/Leapyear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    internal class Leapyear
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Enter a year:  ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + "is not a leap year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- FunctionalProgram/FunctionalProgram/Program.cs
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -7,16 +7,20 @@ using System.Threading.Tasks;
 
 namespace BasicCorePrograms
 {
-    public class BasicPraograms
+    public class BasicPrograms
     {
         static void Main(string[] args)
         {
             Flipcoin flipCoin = new Flipcoin();
             flipCoin.Flip();
+
             Powerof2 powerof2 = new Powerof2();
             Console.WriteLine("\n" + "Enter the power value Up To 31");
             int num = Convert.ToInt32(Console.ReadLine());
             powerof2.Power(num);
+
+            Leapyear leapyear= new Leapyear();
+            leapyear.year();
         }
     }
 }
